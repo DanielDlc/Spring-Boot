82a4634de77735e2a0992fbd024652236a3b76ea
DanielDlc <dlc.engcomputacao@gmail.com>
2022-08-09 07:43:21 -0300

    injeção de dependências Clientes Service

--- Vendas/src/main/java/io/github/danieldlc/service/ClientesService.java
+++ Vendas/src/main/java/io/github/danieldlc/service/ClientesService.java
@@ -2,17 +2,25 @@ package io.github.danieldlc.service;
 
 import io.github.danieldlc.model.Cliente;
 import io.github.danieldlc.repository.ClientesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClientesService {
 
+    private ClientesRepository repository;
+
+    @Autowired
+    public ClientesService(ClientesRepository repository){
+        this.repository = repository;
+    }
+
     public void salvarCliente(Cliente cliente) {
         validarCliente(cliente);
-        ClientesRepository clientesRepository = new ClientesRepository();
+        this.repository.persistir(cliente);
     }
 
     public void validarCliente(Cliente cliente){
-
+        //aplica validações
     }
 }
