9c74d181cc75a86b5b3c75ae308a19b741f01e54
DanielDlc <dlc.engcomputacao@gmail.com>
2022-08-11 10:29:44 -0300

    Clientes Repository

--- Vendas/src/main/java/io/github/danieldlc/repository/ClientesRepository.java
+++ Vendas/src/main/java/io/github/danieldlc/repository/ClientesRepository.java
@@ -1,7 +1,11 @@
 package io.github.danieldlc.repository;
 
+import io.github.danieldlc.model.Cliente;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ClientesRepository {
+    public void persistir(Cliente cliente) {
+        // acessa a base e salva o cliente
+    }
 }
