9174d63b389cbf29f2d651dadbe9f7e183642b1c
DanielDlc <dlc.engcomputacao@gmail.com>
2022-08-11 10:30:10 -0300

    Minha Configuração

--- Vendas/src/main/java/io/github/danieldlc/MinhaConfiguration.java
+++ Vendas/src/main/java/io/github/danieldlc/MinhaConfiguration.java
@@ -1,14 +1,8 @@
 package io.github.danieldlc;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MinhaConfiguration {
-
-    @Bean(name = "applicationName")
-    public String applicationName(){
-        return "Sistema de Vendas";
-    }
 
 }
